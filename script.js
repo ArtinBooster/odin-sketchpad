01551dca2c8ce191b7503c9d6b406c89ced6fd96
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-07-17 14:23:47 -0700

    Make the squares black when user hovers on them

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const containerDivs = document.querySelectorAll(".container");
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
     const newDiv = document.createElement("div");
+    newDiv.classList.add("square");
     containerDivs[i].appendChild(newDiv);
     }
+}
+
+const squareDivs = document.querySelectorAll(".square");
+
+for (let i = 0; i < squareDivs.length; i++) {
+    const currentSquareDiv = squareDivs[i];
+    currentSquareDiv.addEventListener("mouseover", () => {
+        currentSquareDiv.style.backgroundColor = "black";
+    })
 }
