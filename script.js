c17696bd0ae61814a26c42f911266762a53b3316
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-07-17 15:46:11 -0700

    Add "rearrangeButton" for user to change the number of rows of the scale

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
-const containerDivs = document.querySelectorAll(".container");
+const htmlBody = document.querySelector("body");
+const rearrangeButton = document.querySelector(".rearrange-button");
+let numberOfRows = 16;
+let containers;
+let squares;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("square");
-    containerDivs[i].appendChild(newDiv);
+function fillTheScale() {
+    for (let a = 0; a < numberOfRows; a++) {
+        // New containers get created as many as "numberOfRows"
+        const newContainer = document.createElement("div");
+        newContainer.classList.add("container");
+        for (let b = 0; b < numberOfRows; b++) {
+            // New squares get created as many as "numberOfRows"
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("square");
+            // The new square gets appended to the new container
+            newContainer.appendChild(newSquare);
+        }
+        // The new container gets appended to the body tag in "index.HTML"
+        htmlBody.appendChild(newContainer);
+    }
+    containers = document.querySelectorAll(".container");
+    squares = document.querySelectorAll(".square");
+}
+function makeTheSquaresBlackWhenHovered() {
+        for (let i = 0; i < squares.length; i++) {
+        const currentSquare = squares[i];
+        currentSquare.addEventListener("mouseover", () => {
+            currentSquare.style.backgroundColor = "black";
+        })
     }
 }
-
-const squareDivs = document.querySelectorAll(".square");
-
-for (let i = 0; i < squareDivs.length; i++) {
-    const currentSquareDiv = squareDivs[i];
-    currentSquareDiv.addEventListener("mouseover", () => {
-        currentSquareDiv.style.backgroundColor = "black";
-    })
+function emptyTheScale() {
+    for (let a = 0; a < numberOfRows; a++) {
+        const currentContainer = containers[a];
+        htmlBody.removeChild(currentContainer);
+    }
 }
+function getTheNewNumberOfRows() {
+    const userAnswer = parseInt(prompt("How many rows do you want the scale to have? (Between 1 and 100)", "1"));
+    if (
+        userAnswer != NaN &&
+        userAnswer >= 1 &&
+        userAnswer <= 100
+    ) {
+        return userAnswer;
+    } else {
+        alert("The given input wasn't suitable. Please try again.");
+        getTheNewNumberOfRows();
+    }
+}
+function rearrange() {
+    numberOfRows = getTheNewNumberOfRows();
+}
+
+// The scale gets filled with the default "numberOfRows"
+fillTheScale();
+makeTheSquaresBlackWhenHovered();
+// The scale gets reset when user clicks on "rearrangeButton"
+rearrangeButton.addEventListener("click", () => {
+    emptyTheScale();
+    rearrange();
+    fillTheScale();
+    makeTheSquaresBlackWhenHovered();
+})
