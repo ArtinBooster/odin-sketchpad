0119f156739d55dda30e12ad7f62f762f423fda0
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-07-17 10:57:43 -0700

    Add 16 divs to each ".container" div with Javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const containerDivs = document.querySelectorAll(".container");
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+    const newDiv = document.createElement("div");
+    containerDivs[i].appendChild(newDiv);
+    }
+}
